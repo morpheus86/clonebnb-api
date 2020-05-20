d20bd9c03c11e0e7958c0a3b6cacf04e0f6cf9b3
hamadou diallo <hamadou_diallo@yahoo.com>
2020-05-20 12:49:49 -0400

    preparing to deploy

--- index.js
+++ index.js
@@ -21,24 +21,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", routes);
+
 // custom error handling
 app.use((err, req, res, next) => {
-  // just in case
   if (!err.stack || !err.message) next(err);
-  // clean up the trace to just relevant info
+
   const cleanTrace = err.stack
     .split("\n")
     .filter((line) => {
-      // comment out the next two lines for full (verbose) stack traces
-      const projectFile = line.indexOf(__dirname) > -1; // omit built-in Node code
-      const nodeModule = line.indexOf("node_modules") > -1; // omit npm modules
+      const projectFile = line.indexOf(__dirname) > -1;
+      const nodeModule = line.indexOf("node_modules") > -1;
       return projectFile && !nodeModule;
     })
     .join("\n");
-  // colorize and format the output
-  // console.log(chalk.magenta("      " + err.message));
-  // console.log("    " + chalk.gray(cleanTrace));
-  // send back error status
+
   res.status(err.status || 500).end();
 });
 
