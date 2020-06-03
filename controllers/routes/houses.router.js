6a476cdcf67a3daaac187c861b477d8b750312c8
hamadou diallo <hamadou_diallo@yahoo.com>
2020-06-02 20:02:22 -0400

    backend deployment successful

--- controllers/routes/houses.router.js
+++ controllers/routes/houses.router.js
@@ -211,7 +211,7 @@ router.get("/bookings/list/:userId", requireAuth, async (req, res, next) => {
       },
       order: [["startDate", "ASC"]],
     });
-
+    // problem in bookings and how the data comes back Seems like data is not added to booking check algo
     const bookings = await Promise.all(
       bookingData.map(async (book) => {
         return {
